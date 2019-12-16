2af7288dc3a195dfa2f846aed509d0f8b608fd68
tirnavali <tran.ce.co@gmail.com>
2019-12-16 17:17:42 +0300

    Bu proje için ilk commit.

---
+++ src/anasayfa/anasayfaController.java
@@ -0,0 +1,72 @@
+package anasayfa;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import model.DataModel;
+import model.Kisi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class anasayfaController {
+    @FXML
+    TableView<Kisi> kisilerTablosu;
+    @FXML
+    TableColumn<Kisi, String> kisiAd, kisiSoyad, kisiTelefon, kisiBirimAdi;
+
+    @FXML
+    private ImageView fotograf ;
+
+    @FXML
+    private Button button1;
+
+    public void initialize(){
+        Image foto = new Image(getClass().getResourceAsStream("/resources/images/person.png"));
+        fotograf.setImage(foto);
+
+        //this.ListInformation();
+        ObservableList<Kisi> mylist = FXCollections.observableList(DataModel.getInstance().getAllKisiler(DataModel.ORDER_BY_ASC));
+        kisilerTablosu.setItems(mylist);
+        kisiAd.setCellValueFactory(new PropertyValueFactory<Kisi, String>("adProperty"));
+        kisiSoyad.setCellValueFactory(new PropertyValueFactory<Kisi, String>("soyad"));
+        kisiTelefon.setCellValueFactory(new PropertyValueFactory<Kisi, String>("telefon"));
+        kisiBirimAdi.setCellValueFactory(new PropertyValueFactory<Kisi, String>("eposta"));
+
+
+
+    }
+    public void ListInformation(){
+        System.out.println("list information methodu çalıştı");
+        Task<ObservableList<Kisi>> task = new GetAllPeopleTask();
+        new Thread(task).start();
+
+        kisilerTablosu.setItems(task.getValue());
+       kisiAd.setCellValueFactory(new PropertyValueFactory<>("ad"));
+
+
+
+
+
+        System.out.println("list information methodu bitti");
+    }
+
+    class GetAllPeopleTask extends Task{
+        @Override
+        protected ObservableList call() throws Exception {
+            return FXCollections.observableArrayList(DataModel.getInstance().getAllKisiler(DataModel.ORDER_BY_ASC));
+        }
+    }
+
+
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module JavaFxApplication {
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.sql;
+
+    opens anasayfa;
+}
