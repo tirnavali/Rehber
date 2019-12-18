0937e0d3d1027e8bb597d0f54d114eda165bad99
tirnavali <tran.ce.co@gmail.com>
2019-12-18 11:02:36 +0300

    Proje maven projesine çevrildi.

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module JavaFxApplication {
-    requires javafx.fxml;
-    requires javafx.base;
-    requires javafx.graphics;
-    requires javafx.controls;
-    requires java.sql;
-
-    opens anasayfa;
-}
